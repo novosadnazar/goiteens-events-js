64085871b522d5b9db9c50a5b39221fabf733513
novosadnazar <163895331+novosadnazar@users.noreply.github.com>
2026-02-23 17:58:44 +0200

    fix

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -1,6 +1,12 @@
 import { Item } from "./Event.style";
 import { Text } from "./Event.style"
 import { Desc } from "./Event.style"
+import { FaLocationDot } from "react-icons/fa6";
+import { IoPeople } from "react-icons/io5";
+import { FaRegCalendarAlt } from "react-icons/fa";
+import {IoIosTimer} from "react-icons/io";
+
+
 export const Event = ({ props }) => {
   return (
     <>
@@ -8,11 +14,11 @@ export const Event = ({ props }) => {
         return (
           <Item key={name}>
             <Text>{name}</Text>
-            <Desc>{location}</Desc>
-            <Desc>{speaker}</Desc>
-            <Desc>{type}</Desc>
-            <Desc>{end}</Desc>
-            <Desc>{start}</Desc>
+            <Desc> <FaLocationDot /> {location}</Desc>
+            <Desc> <IoPeople /> {speaker}</Desc>
+            <Desc> <FaRegCalendarAlt />{type}</Desc>
+            <Desc> <IoIosTimer />{end}</Desc>
+            <Desc> <IoIosTimer />{start}</Desc>
           </Item>
         );
       })}
